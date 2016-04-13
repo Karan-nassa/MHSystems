4ea60e16e610049651c61a1714e3c6fae2d2f567
Karan Nassa <karan@ucreate.co.in>
2016-04-13 20:07:12 +0530

    Update Members list
    Today, I have worked on following tasks :
    1.) [DONE] R&D and implementation of list VARIENT in android same as
    displaying for Members in this link
    "https://marvelapp.com/120fj45#11568008" .
    2.) [DONE] Display SPINNER [Drop-down] on tool bar with three items
    named "All", "Ladies", "Gentlemens".
    3.) [DONE] Display SEARCH and MORE icon on right side on Members screen.
    
    BUILD:
    https://www.dropbox.com/s/9nrntzyhn1n1jka/MHSystem_13.04.2016.apk?dl=0

--- MHSystems/app/src/main/java/com/ucreate/mhsystems/activites/MembersActivity.java
+++ MHSystems/app/src/main/java/com/ucreate/mhsystems/activites/MembersActivity.java
@@ -2,17 +2,24 @@ package com.ucreate.mhsystems.activites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ucreate.mhsystems.R;
 import com.ucreate.mhsystems.adapter.BaseAdapter.AlphabetListAdapter;
+import com.ucreate.mhsystems.adapter.BaseAdapter.CustomSpinnerAdapter;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -27,220 +34,113 @@ public class MembersActivity extends AppCompatActivity {
      * DECLARATION OF CONSTANTS
      *******************************/
 
-    private int sideIndexHeight;
-    private static float sideIndexX;
-    private static float sideIndexY;
-    private int indexListSize;
 
     /*********************************
      * INSTANCES OF CLASSES
      *******************************/
-    ListView lvMembersList;
-
-    private AlphabetListAdapter adapter = new AlphabetListAdapter();
-    private GestureDetector mGestureDetector;
-    private List<Object[]> alphabet = new ArrayList<Object[]>();
-    private HashMap<String, Integer> sections = new HashMap<String, Integer>();
-
-
-    /**
-     * Declaration of Side alphabet indexing gesture.
-     */
-    class SideIndexGestureListener extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            sideIndexX = sideIndexX - distanceX;
-            sideIndexY = sideIndexY - distanceY;
-
-            if (sideIndexX >= 0 && sideIndexY >= 0) {
-                displayListItem();
-            }
-
-            return super.onScroll(e1, e2, distanceX, distanceY);
-        }
-    }
+    private Toolbar toolbar;
+    private Spinner spinner_nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
 
-        mGestureDetector = new GestureDetector(this, new SideIndexGestureListener());
 
-        lvMembersList = (ListView) findViewById(R.id.lvMembersList);
+        setupToolBar();
+    }
 
-        List<String> countries = populateCountries();
-        Collections.sort(countries);
+    private void setupToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBarMembers);
+        spinner_nav = (Spinner) findViewById(R.id.spinner_nav);
 
-        List<AlphabetListAdapter.Row> rows = new ArrayList<AlphabetListAdapter.Row>();
-        int start = 0;
-        int end = 0;
-        String previousLetter = null;
-        Object[] tmpIndexItem = null;
-        Pattern numberPattern = Pattern.compile("[0-9]");
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        for (String country : countries) {
-            String firstLetter = country.substring(0, 1);
-
-            // Group numbers together in the scroller
-            if (numberPattern.matcher(firstLetter).matches()) {
-                firstLetter = "#";
-            }
-
-            // If we've changed to a new letter, add the previous letter to the alphabet scroller
-            if (previousLetter != null && !firstLetter.equals(previousLetter)) {
-                end = rows.size() - 1;
-                tmpIndexItem = new Object[3];
-                tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
-                tmpIndexItem[1] = start;
-                tmpIndexItem[2] = end;
-                alphabet.add(tmpIndexItem);
-
-                start = end + 1;
-            }
-
-            // Check if we need to add a header row
-            if (!firstLetter.equals(previousLetter)) {
-                rows.add(new AlphabetListAdapter.Section(firstLetter));
-                sections.put(firstLetter, start);
-            }
-
-            // Add the country to the list
-            rows.add(new AlphabetListAdapter.Item(country));
-            previousLetter = firstLetter;
         }
+        addItemsToSpinner();
+    }
 
-        if (previousLetter != null) {
-            // Save the last letter
-            tmpIndexItem = new Object[3];
-            tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
-            tmpIndexItem[1] = start;
-            tmpIndexItem[2] = rows.size() - 1;
-            alphabet.add(tmpIndexItem);
-        }
+    // add items into spinner dynamically
+    public void addItemsToSpinner() {
 
-        adapter.setRows(rows);
-        lvMembersList.setAdapter(adapter);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("All");
+        list.add("Ladies");
+        list.add("Gentlemens");
 
-        updateList();
+        // Custom ArrayAdapter with spinner item layout to set popup background
+
+        CustomSpinnerAdapter spinAdapter = new CustomSpinnerAdapter(
+                getApplicationContext(), list);
+
+
+
+        // Default ArrayAdapter with default spinner item layout, getting some
+        // view rendering problem in lollypop device, need to test in other
+        // devices
+
+  /*
+   * ArrayAdapter<String> spinAdapter = new ArrayAdapter<String>(this,
+   * android.R.layout.simple_spinner_item, list);
+   * spinAdapter.setDropDownViewResource
+   * (android.R.layout.simple_spinner_dropdown_item);
+   */
+
+        spinner_nav.setAdapter(spinAdapter);
+
+        spinner_nav.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View v,
+                                       int position, long id) {
+                // On selecting a spinner item
+                String item = adapter.getItemAtPosition(position).toString();
+
+                // Showing selected spinner item
+//                Toast.makeText(getApplicationContext(), "Selected  : " + item,
+//                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        });
+
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (mGestureDetector.onTouchEvent(event)) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_members, menu);
+        return true;
     }
 
-    public void updateList() {
-        LinearLayout sideIndex = (LinearLayout) findViewById(R.id.llSideIndex);
-        sideIndex.removeAllViews();
-        indexListSize = alphabet.size();
-        if (indexListSize < 1) {
-            return;
-        }
-
-        int indexMaxSize = (int) Math.floor(sideIndex.getHeight() / 20);
-        int tmpIndexListSize = indexListSize;
-        while (tmpIndexListSize > indexMaxSize) {
-            tmpIndexListSize = tmpIndexListSize / 2;
-        }
-        double delta;
-        if (tmpIndexListSize > 0) {
-            delta = indexListSize / tmpIndexListSize;
-        } else {
-            delta = 1;
-        }
-
-        TextView tmpTV;
-        for (double i = 1; i <= indexListSize; i = i + delta) {
-            Object[] tmpIndexItem = alphabet.get((int) i - 1);
-            String tmpLetter = tmpIndexItem[0].toString();
-
-            tmpTV = new TextView(this);
-            tmpTV.setText(tmpLetter);
-            tmpTV.setGravity(Gravity.CENTER);
-            tmpTV.setTextSize(15);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
-            tmpTV.setLayoutParams(params);
-            sideIndex.addView(tmpTV);
-        }
-
-        sideIndexHeight = sideIndex.getHeight();
-
-        sideIndex.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // now you know coordinates of touch
-                sideIndexX = event.getX();
-                sideIndexY = event.getY();
-
-                // and can display a proper item it country list
-                displayListItem();
-
-                return false;
-            }
-        });
-    }
-
-    public void displayListItem() {
-        LinearLayout llSideIndex = (LinearLayout) findViewById(R.id.llSideIndex);
-        sideIndexHeight = llSideIndex.getHeight();
-        // compute number of pixels for every side index item
-        double pixelPerIndexItem = (double) sideIndexHeight / indexListSize;
-
-        // compute the item index for given event position belongs to
-        int itemPosition = (int) (sideIndexY / pixelPerIndexItem);
-
-        // get the item (we can do it since we know item index)
-        if (itemPosition < alphabet.size()) {
-            Object[] indexItem = alphabet.get(itemPosition);
-            int subitemPosition = sections.get(indexItem[0]);
-
-            //ListView listView = (ListView) findViewById(android.R.id.list);
-            lvMembersList.setSelection(subitemPosition);
-        }
-    }
-
-    private List<String> populateCountries() {
-        List<String> countries = new ArrayList<String>();
-        countries.add("Abas Mr Hassan");
-        countries.add("Abecasis Miss Nathalie");
-        countries.add("Abecasis Mr Sebastian");
-        countries.add("Bain Mr William");
-        countries.add("Barry-Walsh Mr Edward");
-        countries.add("Zuill Mr Stewart");
-        countries.add("Zimmerman Mr Stephen");
-        countries.add("Yurko Mr Allen");
-        countries.add("Young Mr Robin");
-        countries.add("Willian Mr John Stuart");
-        countries.add("Turner Mr Roger");
-        countries.add("Sturgeon Mr Benjamin");
-        countries.add("Spencer Mr Callum");
-        countries.add("Men");
-        countries.add("Sharp Mr William");
-        countries.add("Roy Mrs Lesley Ann");
-        countries.add("Posford Mr Stephen");
-        countries.add("Oldmeadow Mr Charles");
-        countries.add("Naylor-Leyland Mrs Jane");
-        countries.add("Naylor-Leyland Mr David");
-        countries.add("Nash Lord John");
-        countries.add("Murr Mr Michael");
-        countries.add("Lynch Mr Alexander");
-        countries.add("Keenan Mr Farren");
-        countries.add("Keenan Miss Ellis");
-        countries.add("Holland Mr Charles");
-        countries.add("Holland Mr Charles");
-        countries.add("Grant Mr Hugh");
-        countries.add("Foster Mr Christopher");
-        countries.add("Fernandez Mr Luis Javier");
-        countries.add("Edgeworth Jr. Mr James");
-        countries.add("Durrant Mr Brian");
-        countries.add("Caldwell Mr Philip");
-        countries.add("Caldwell Mr Richard");
-        countries.add("JOHNBARTON");
-        return countries;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_today) {
+//            Toast.makeText(getApplicationContext(), "Settings Clicked",
+//                    Toast.LENGTH_SHORT).show();
+//            return true;
+//        } else if (id == R.id.action_search) {
+//            Toast.makeText(getApplicationContext(), "Search Clicked",
+//                    Toast.LENGTH_SHORT).show();
+//            return true;
+//        } else if (id == R.id.action_add) {
+//            Toast.makeText(getApplicationContext(), "Add Clicked",
+//                    Toast.LENGTH_SHORT).show();
+//            return true;
+//        } else if (id == R.id.action_delete) {
+//            Toast.makeText(getApplicationContext(), "Delete Clicked",
+//                    Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
+        return super.onOptionsItemSelected(item);
     }
 }
