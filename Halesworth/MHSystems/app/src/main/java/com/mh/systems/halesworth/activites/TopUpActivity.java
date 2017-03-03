ebcc7255ee272bd4cbc315df1d74fc8bd057ef42
Karan Nassa <karan@ucreate.co.in>
2017-03-03 17:42:14 +0530

    Added Version name in settings and check extra price in Top up in Halesworth.
     - UPDATE: Get extra deduction amount from key 'TopupTxFeeStr' in ToUpActivity along with pricing list. Also display deduction amount before make payment if exists.
     - NEW: Display VERSION NAME in the Settings screen at bottom.

--- Halesworth/MHSystems/app/src/main/java/com/mh/systems/halesworth/activites/TopUpActivity.java
+++ Halesworth/MHSystems/app/src/main/java/com/mh/systems/halesworth/activites/TopUpActivity.java
@@ -1,6 +1,8 @@
 package com.mh.systems.halesworth.activites;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -30,6 +32,7 @@ import com.mh.systems.halesworth.constants.ApplicationGlobal;
 import com.mh.systems.halesworth.models.TopUp.TopUpPriceListResponse;
 import com.mh.systems.halesworth.models.TopUp.TopUpPricesListAPI;
 import com.mh.systems.halesworth.models.TopUp.TopupList;
+import com.mh.systems.halesworth.web.WebAPI;
 import com.mh.systems.halesworth.web.api.WebServiceMethods;
 import com.newrelic.com.google.gson.Gson;
 import com.newrelic.com.google.gson.reflect.TypeToken;
@@ -140,16 +143,22 @@ public class TopUpActivity extends BaseActivity {
         @Override
         public void onClick(View v) {
 
-            if(etInputPrize.getText().toString().length() > 0) {
+            if (etInputPrize.getText().toString().length() > 0) {
 
                 if (isOnline(TopUpActivity.this)) {
 
                     if (fTopUpPrize >= iMinTopup && fTopUpPrize <= iMaxTopup) {
-                        intent = new Intent(TopUpActivity.this, MakePaymentWebActivity.class);
-                        intent.putExtra("fTopUpPrize", fTopUpPrize);
-                        intent.putExtra("fCardBalance", fCardBalance);
-                        intent.putExtra("strCurrencySign", tvCurrencySign.getText().toString());
-                        startActivityForResult(intent, ACTION_MAKE_PAYMENT);
+
+                        /**
+                         * Implements check if any extra charges deduct in payment then
+                         * show it to user first. If user agree then proceed to payment.
+                         */
+                        if (topUpPriceListResponse.getData().getTopupTxFeeStr().length() > 0) {
+                            proceedToNextStep();
+                        } else {
+                            proceedToPayment();
+                        }
+
                     } else {
                         showAlertMessage("Top Up range should remain between " + strMinTopup + " and " + strMaxTopup + ".");
                     }
@@ -157,11 +166,47 @@ public class TopUpActivity extends BaseActivity {
                     //showAlertMessage(getString(R.string.error_no_connection));
                     showAlertMessage(getString(R.string.error_no_internet));
                 }
-            }else{
+            } else {
                 showAlertMessage(getString(R.string.error_enter_topup));
             }
         }
     };
+
+    /**
+     * Finally, proceeds to make payment.
+     */
+    private void proceedToPayment() {
+        intent = new Intent(TopUpActivity.this, MakePaymentWebActivity.class);
+        intent.putExtra("fTopUpPrize", fTopUpPrize);
+        intent.putExtra("fCardBalance", fCardBalance);
+        intent.putExtra("strCurrencySign", tvCurrencySign.getText().toString());
+        startActivityForResult(intent, ACTION_MAKE_PAYMENT);
+    }
+
+    /**
+     * Implements this method to proceed the next step of
+     * FSI payment. For example : If "TopupTxFeeStr" key is not
+     * empty then display user about deduct extra charges.
+     */
+    private void proceedToNextStep() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("");
+        builder.setMessage(topUpPriceListResponse.getData().getTopupTxFeeStr())
+                .setCancelable(false)
+                .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        proceedToPayment();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 
     private TextWatcher mPrizeChangeListener = new TextWatcher() {
         @Override
@@ -263,7 +308,7 @@ public class TopUpActivity extends BaseActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         /**
-         * If PAYMENT status SUCCESS then navigate user to {@link com.mh.systems.halesworth.fragments.FinanceFragment}
+         * If PAYMENT status SUCCESS then navigate user to {@link com.mh.systems.sunningdale.fragments.FinanceFragment}
          * Otherwise, retain on this screen.
          */
         if (resultCode == ACTION_MAKE_PAYMENT) {
@@ -292,7 +337,7 @@ public class TopUpActivity extends BaseActivity {
 
         //Creating a rest adapter
         RestAdapter adapter = new RestAdapter.Builder()
-                .setEndpoint("https://staging.mhsystems.co.uk/")
+                .setEndpoint(WebAPI.API_BASE_URL)
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .build();
 
