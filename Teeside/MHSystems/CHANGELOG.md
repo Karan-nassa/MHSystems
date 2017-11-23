6870e0b0d05cf9513b9ce8d18a5c49e372d3f8dd
Karan Nassa <karan@ucreate.co.in>
2017-11-23 12:32:21 +0530

    Copy of MHS GOLF to Teesside Golf Club.
    - Competitions : Add Option to choose which competitions to display
    - Competition Results : Display All results in collapsible way
    - Mobile App Item 131: URGENT BUG. Handicap Info not Correct

--- Teeside/MHSystems/CHANGELOG.md
+++ Teeside/MHSystems/CHANGELOG.md
@@ -1,3 +1,8 @@
 ##Version 1.0.0 ()
 
 Initial release.
+
+ ####22nd Nov, 2017
+   - Competitions : Add Option to choose which competitions to display
+   - Competition Results : Display All results in collapsible way 
+   - Mobile App Item 131: URGENT BUG. Handicap Info not Correct
---
+++ Teeside/MHSystems/app/src/main/java/com/mh/systems/teesside/web/models/teetimebooking/getdaydata/AJsonParamsGetDayData.java
@@ -0,0 +1,63 @@
+
+package com.mh.systems.teesside.web.models.teetimebooking.getdaydata;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AJsonParamsGetDayData {
+
+    @SerializedName("Version")
+    @Expose
+    private int version;
+    @SerializedName("MemberId")
+    @Expose
+    private String memberId;
+    @SerializedName("datefrom")
+    @Expose
+    private String datefrom;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public AJsonParamsGetDayData() {
+    }
+
+    /**
+     * 
+     * @param datefrom
+     * @param memberId
+     * @param version
+     */
+    public AJsonParamsGetDayData(int version, String memberId, String datefrom) {
+        super();
+        this.version = version;
+        this.memberId = memberId;
+        this.datefrom = datefrom;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getDatefrom() {
+        return datefrom;
+    }
+
+    public void setDatefrom(String datefrom) {
+        this.datefrom = datefrom;
+    }
+
+}
